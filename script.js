013cf1bd2728b499d741413120b182441d85a953
ishanjain126 <jain.ishan126@gmail.com>
2021-07-19 21:43:35 +0530

    commit 1

---
+++ script.js
@@ -0,0 +1,12 @@
+function valuecheck(){
+    // alert("Hello there!")
+    const val = document.getElementById("checkName").value.toLowerCase()
+    console.log("value", val)
+
+    if (val === "nemo"){
+        return alert("Wohoo Congrats Nemo!")
+    }
+    else{
+        return alert("Ehhh, wrong name ! ")
+    }
+}
