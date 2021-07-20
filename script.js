d67cabe6a0e0685de4a193bb67c73849befb2dec
ishanjain126 <jain.ishan126@gmail.com>
2021-07-20 06:46:23 +0530

    commit 5

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function valuecheck(){
     // alert("Hello there!")
-    const val = document.getElementById("checkName").value.toLowerCase()
+    const val = document.getElementById("checkName").value.trim().toLowerCase()
     console.log("value", val)
 
     if (val === "nemo"){
-        return alert("yo Congrats Nemo! All the best now !")
+        return alert("yo Congrats Nemo !")
     }
     else{
         return alert("Ehhh, wrong name! You can do better. ")
